42dfa93732c6a702257dc407761c22362d99ab32
Ashutosh Kumar <raj.fungus@gmail.com>
2020-05-04 23:37:13 +0530

    Register ROUTES

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,29 +1,47 @@
-const express =require('express')
-const shortid = require('shortid')
-const chalk = require('chalk')
+const express = require('express');
+const shortid = require('shortid');
+const chalk = require('chalk');
+const bcrypt = require('bcrypt')
 
 const router = express.Router();
-const shorturlCollection = require('../models/shorturls')
+const shorturlCollection = require('../models/shorturls');
+const userCollection = require('../models/users');
 
-
-router.post('/addURL', async (req,res)=>{
+router.post('/addURL', async (req, res) => {
     let urlData = req.body;
     urlData.shortURL = shortid.generate();
-    const shortURL = await shorturlCollection.findOne({customShortURL:urlData.customShortURL});
-    if(shortURL===null){
-        new shorturlCollection(urlData).save((err)=>{
-            if(err)
-                console.log(chalk.red('Error in Inserting Data'))
-            else{
-                console.log(chalk.green('Data Inserted'))
-                res.send({done:true,message:"succesfully inserted data"})
+    const shortURL = await shorturlCollection.findOne({ customShortURL: urlData.customShortURL });
+    if (shortURL === null) {
+        new shorturlCollection(urlData).save((err) => {
+            if (err) console.log(chalk.red('Error in Inserting Data'));
+            else {
+                console.log(chalk.green('Data Inserted'));
+                res.send({ done: true, message: 'succesfully inserted data' });
             }
-        })
+        });
+    } else {
+        res.send({ done: false, message: 'customURL already Exists', status: 1901 });
     }
-    else{
-        res.send({done:false,message:'customURL already Exists',status:1901})
+});
+router.post('/register', async (req, res) => {
+    let userData = req.body;
+    userData.password = await bcrypt.hash(userData.password,10)
+    const user = await userCollection.findOne({ $or: [{ username: userData.username }, { email: userData.email }] });
+    if (user === null) {
+        new userCollection(userData).save((err) => {
+            if (err) console.log(chalk.red('Unknown error in Inserting Data'));
+            else {
+                console.log(chalk.green('Data inserted'));
+                res.send({ done: true, message: 'successfully inserted data' });
+            }
+        });
+    } else {
+        if (user.email === userData.email)
+            res.send({ done: false, message: 'An account with this Email already exist.' });
+        else if (user.username === userData.username)
+            res.send({ done: false, message: 'An account with the username already exists' });
+        else res.send({ done: false, message: 'Unknown error in Inserting data' });
     }
-    
-})
+});
 
 module.exports = router;
