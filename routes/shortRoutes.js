764245ab11fcf1d3fc4966a5a14f8087f780c82d
Ashutosh Kumar <raj.fungus@gmail.com>
2020-05-09 19:49:33 +0530

    Succesful ShortURL new Logic

--- routes/shortRoutes.js
+++ routes/shortRoutes.js
@@ -64,7 +64,7 @@ function redirectShort(shortURL,res) {
 }
 
 function compareDates(d1,d2) {
-    if(d1.getFullYear() === d2.getFullYear() && d1.getMonth()==d2.getMonth() && d2.getDate()){
+    if(d1.getFullYear() === d2.getFullYear() && d1.getMonth()===d2.getMonth() && d1.getDate()===d2.getDate()){
         return true;
     }
     else{
