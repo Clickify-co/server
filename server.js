e1b35c32c77061f2aeac62541a9d358133626414
Ashutosh Kumar <ashutosh.kumar@forty4hz.com>
2021-09-11 23:09:26 +0530

    'TitleBar'

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require('dotenv').config(); //Environment Configs
 //Configure Limiter
 const limiter = rateLimit({
 	windowMs: 2 * 60 * 1000, // 15 minutes
-	max: 100 // limit each IP to 100 requests per windowMs
+	max: 5 // limit each IP to 100 requests per windowMs
   });
 
 // CONFIGURE SENTRY
